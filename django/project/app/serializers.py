b4cc05a0e67f7858db565b59ea61bfa838d781a7
Jidesh Baidya <jideshvaidya@gmail.com>
2019-02-12 19:50:17 +0545

    register

---
+++ django/project/app/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password' : {'write_only' : True, 'required': True}}
+
+
+    def create(self, validate_data):
+        user = User.objects.create_user(**validate_data)
+        return user
